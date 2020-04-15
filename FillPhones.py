c3d3cbeeed0b981012115f2c070f7b150eb8a3cf
Alex <alexandr.podchasov@yandex.ru>
2020-04-15 12:07:23 +0300

    Patch 0.0.003
    General changes, improvements and fixes:
    - Added OTRS web-parsing. Now script can find employee-table of any company web-page.
    - Added Confluence and OTRS employee-tables data preparing.
    
    Known issues:
    - On Confluence employee-table data preparing, if the E-mail column is empty, then the data is copied from the second table without comparison.

--- FillPhones.py
+++ FillPhones.py
@@ -5,16 +5,18 @@ import io
 import codecs
 import html2text
 import codecs
+import os
+import shutil
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-import os
 from getpass import getpass
 import pyautogui,time
 import pandas as pd
+import numpy as np
 
 
 BLOCKSIZE = 1048576
@@ -26,11 +28,11 @@ j = 0
 firstClient = 'randomword'
 flag = False
 temppath = os.getenv('TEMP')
-
-
-#get html web-page
 browser = webdriver.Chrome('chromedriver.exe')
-r = browser.get('https://wiki.itfb.ru/pages/viewpage.action?pageId=13894474')
+
+
+# Confluence parsing and tables preparing
+browser.get('https://wiki.itfb.ru/pages/viewpage.action?pageId=13894474')
 pyautogui.keyDown('alt')
 pyautogui.press('tab')
 pyautogui.keyUp('alt')
@@ -41,23 +43,102 @@ s_username.send_keys(str(input('Введите логин: ')))
 s_password.send_keys(str(getpass('Введите пароль: ')))
 s_continue.click()
 page = browser.page_source
-file = codecs.open(temppath + '\clients.html', 'w', encoding = 'utf-8')
+try:
+    os.mkdir(temppath + "\FillPhones");
+except OSError:
+    shutil.rmtree(temppath + "\FillPhones");
+    os.mkdir(temppath + "\FillPhones");
+file = codecs.open(temppath + '\FillPhones\ADT_conf_raw.html', 'w', encoding = 'utf-8')
 file.write(page)
 file.close()
-with open(temppath + '\clients.html', 'r', encoding = 'utf-8') as file:
+with open(temppath + '\FillPhones\ADT_conf_raw.html', 'r', encoding = 'utf-8') as file:
     for line in file:
         if "Телефоны сотрудников" in line:
             start_table = line.find("Телефоны сотрудников")
             end_table = line.find("2. Доступ в Интернет/Телефония", start_table - 1)
-            print(start_table)
-            print(end_table)
             out = line[start_table:end_table]
-            with open(temppath + '\clients1.html', 'w', encoding = 'utf-8') as file1:
+            with open(temppath + '\FillPhones\ADT_conf_staff_raw.html', 'w', encoding = 'utf-8') as file1:
                 file1.write(out + "\n")           
-tables = pd.read_html(temppath + '\clients1.html')
-print(tables[0].iloc[:,1:6])
-browser.close()
+conf_staff_raw_html = pd.read_html(temppath + '\FillPhones\ADT_conf_staff_raw.html', encoding = 'utf-8')
+conf_staff_raw_xlsx = conf_staff_raw_html[0].drop(conf_staff_raw_html[0].columns[[0, 4, 6, 7]], axis='columns')
+conf_staff_raw_xlsx.to_excel(temppath + '\FillPhones\ADT_conf_staff_raw.xlsx', index=False)
+conf_staff_split_xlsx = conf_staff_raw_xlsx['Ф.И.О.'].str.split(' ',expand=True)
+conf_staff_split_xlsx.to_html(temppath + '\FillPhones\ADT_conf_staff_split.html', index=False)
+conf_staff_split_html = pd.read_html(temppath + '\FillPhones\ADT_conf_staff_split.html', encoding = 'utf-8')
+conf_staff_names = conf_staff_split_html[0].drop(conf_staff_split_html[0].columns[[2]], axis='columns')
+conf_staff_names.to_excel(temppath + '\FillPhones\ADT_conf_staff_names.xlsx', index=False)
+conf_staff_info = conf_staff_raw_html[0].drop(conf_staff_raw_html[0].columns[[0, 1, 4, 6, 7]], axis='columns')
+conf_staff_info.to_excel(temppath + '\FillPhones\ADT_conf_staff_info.xlsx', index=False)
+conf_staff_login = pd.read_excel(temppath + '\FillPhones\ADT_conf_staff_names.xlsx', encoding = 'utf-8')
+conf_staff_login['Логин'] = ""
+conf_staff = conf_staff_login.drop(conf_staff_login.columns[[0, 1]], axis='columns')
+conf_staff = conf_staff.merge(conf_staff_names, left_index=True, right_index=True)
+conf_staff = conf_staff.merge(conf_staff_info, left_index=True, right_index=True)
+conf_staff.to_excel(temppath + '\FillPhones\ADT_conf_staff.xlsx', index=False, header=["Логин", "Фамилия", "Имя", "Внутренний номер", "E-mail", "Моб. номер"])
 
-#deleting temp files
-os.remove(temppath + '\clients.html')
-os.remove(temppath + '\clients1.html')
+# OTRS parsing and tables preparing
+browser.get('https://hd.itfb.ru/index.pl?Action=AdminCustomerUser;Nav=adt')
+s_username = browser.find_element_by_id('User')
+s_password = browser.find_element_by_id('Password')
+s_continue = browser.find_element_by_id('LoginButton')
+s_username.send_keys(str(input('Введите логин: ')))
+s_password.send_keys(str(getpass('Введите пароль: ')))
+s_continue.click()
+company_name = browser.find_element_by_xpath('//*[@id="Search"]')
+company_search = browser.find_element_by_xpath('//form[contains(@class, "SearchBox")]//button[contains(@title,"Поиск")]')
+browser.execute_script("arguments[0].click();", company_name)
+browser.execute_script("arguments[0].value = 'ADT';", company_name);
+browser.execute_script("arguments[0].click();", company_search)
+page = browser.page_source
+otrs_staff_raw_html = pd.read_html(browser.find_element_by_id("CustomerTable").get_attribute('outerHTML'))
+otrs_staff_raw_xlsx = otrs_staff_raw_html[0].drop(otrs_staff_raw_html[0].columns[[3, 4, 5]], axis='columns')
+otrs_staff_raw_xlsx.to_excel(temppath + '\FillPhones\ADT_otrs_staff_raw.xlsx', index=False)
+otrs_staff_split_xlsx = otrs_staff_raw_xlsx['Name - Название'].str.split(' ',expand=True)
+otrs_staff_split_xlsx.to_html(temppath + '\FillPhones\ADT_otrs_staff_split.html', index=False)
+otrs_staff_split_html = pd.read_html(temppath + '\FillPhones\ADT_otrs_staff_split.html', encoding = 'utf-8')
+otrs_staff_names_part1 = otrs_staff_split_html[0].drop(otrs_staff_split_html[0].columns[[0, 2]], axis='columns')
+otrs_staff_names_part2 = otrs_staff_split_html[0].drop(otrs_staff_split_html[0].columns[[1, 2]], axis='columns')
+otrs_staff_names_part1.to_excel(temppath + '\FillPhones\ADT_otrs_staff_names_part1.xlsx', index=False)
+otrs_staff_names_part2.to_excel(temppath + '\FillPhones\ADT_otrs_staff_names_part2.xlsx', index=False)
+otrs_staff_names = otrs_staff_names_part1.merge(otrs_staff_names_part2, left_index=True, right_index=True)
+otrs_staff_names.to_excel(temppath + '\FillPhones\ADT_otrs_staff_names.xlsx', header=False, index=False)
+otrs_staff_login = otrs_staff_raw_html[0].drop(otrs_staff_raw_html[0].columns[[1, 2, 3, 4, 5]], axis='columns')
+otrs_staff_email = otrs_staff_raw_html[0].drop(otrs_staff_raw_html[0].columns[[0, 1, 3, 4, 5]], axis='columns')
+otrs_staff = otrs_staff_login.merge(otrs_staff_names, left_index=True, right_index=True)
+otrs_staff['Внутренний номер'] = ""
+otrs_staff = otrs_staff.merge(otrs_staff_email, left_index=True, right_index=True)
+otrs_staff['Моб. номер'] = ""
+otrs_staff.to_excel(temppath + '\FillPhones\ADT_otrs_staff.xlsx', index=False, header=["Логин", "Фамилия", "Имя", "Внутренний номер", "E-mail", "Моб. номер"])
+
+# Tables merging
+conf_staff = pd.read_excel(temppath + '\FillPhones\ADT_conf_staff.xlsx', encoding = 'utf-8')
+otrs_staff = pd.read_excel(temppath + '\FillPhones\ADT_otrs_staff.xlsx', encoding = 'utf-8')
+staff = conf_staff.merge(otrs_staff, on=['Фамилия', 'Имя'], how = 'outer')
+staff.to_excel(temppath + '\FillPhones\ADT_staff_merge1.xlsx', index=False)
+staff['Логин_x'] = staff[['Логин_x','Логин_y']].fillna('').sum(axis=1)
+staff['Внутренний номер_x'] = staff['Внутренний номер_x'].fillna(0).astype(int)
+staff['Внутренний номер_x'] = staff[['Внутренний номер_x','Внутренний номер_y']].sum(axis=1)
+staff['Внутренний номер_x'] = staff['Внутренний номер_x'].replace(0, np.nan, regex=True)
+staff['E-mail_x'] = staff['E-mail_x'].fillna(staff['E-mail_y'])
+staff['Моб. номер_x'] = staff[['Моб. номер_x','Моб. номер_y']].fillna('').sum(axis=1)
+staff.to_excel(temppath + '\FillPhones\ADT_staff_merge2.xlsx', index=False)
+staff = staff.drop(staff.columns[[6, 7, 8, 9]], axis='columns')
+staff.to_excel(temppath + '\FillPhones\ADT_staff.xlsx', index=False, header=["Логин", "Фамилия", "Имя", "Внутренний номер", "E-mail", "Моб. номер"])
+
+browser.close()
+browser.quit()
+
+# Deleting temp files
+os.remove(temppath + '\FillPhones\ADT_conf_raw.html')
+os.remove(temppath + '\FillPhones\ADT_conf_staff_raw.html')
+os.remove(temppath + '\FillPhones\ADT_conf_staff_raw.xlsx')
+os.remove(temppath + '\FillPhones\ADT_conf_staff_split.html')
+os.remove(temppath + '\FillPhones\ADT_conf_staff_names.xlsx')
+os.remove(temppath + '\FillPhones\ADT_conf_staff_info.xlsx')
+os.remove(temppath + '\FillPhones\ADT_otrs_staff_raw.xlsx')
+os.remove(temppath + '\FillPhones\ADT_otrs_staff_split.html')
+os.remove(temppath + '\FillPhones\ADT_otrs_staff_names_part1.xlsx')
+os.remove(temppath + '\FillPhones\ADT_otrs_staff_names_part2.xlsx')
+os.remove(temppath + '\FillPhones\ADT_otrs_staff_names.xlsx')
+os.remove(temppath + '\FillPhones\ADT_staff_merge1.xlsx')
+os.remove(temppath + '\FillPhones\ADT_staff_merge2.xlsx')
---
+++ FillPhones_offline.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import io
+import codecs
+import html2text
+import codecs
+import os
+import shutil
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from getpass import getpass
+import pyautogui,time
+import pandas as pd
+import numpy as np
+
+
+BLOCKSIZE = 1048576
+str1 = '['
+str2 = '.'
+str3 = '<SelectedValues>'
+k = 0
+j = 0
+firstClient = 'randomword'
+flag = False
+temppath = os.getenv('TEMP')
+#browser = webdriver.Chrome('chromedriver.exe')
+
+
+## Confluence parsing and tables preparing
+#browser.get('https://wiki.itfb.ru/pages/viewpage.action?pageId=13894474')
+#pyautogui.keyDown('alt')
+#pyautogui.press('tab')
+#pyautogui.keyUp('alt')
+#s_username = browser.find_element_by_id('os_username')
+#s_password = browser.find_element_by_id('os_password')
+#s_continue = browser.find_element_by_id('loginButton')
+#s_username.send_keys(str(input('Введите логин: ')))
+#s_password.send_keys(str(getpass('Введите пароль: ')))
+#s_continue.click()
+#page = browser.page_source
+#try:
+#    os.mkdir(temppath + "\FillPhones");
+#except OSError:
+#    shutil.rmtree(temppath + "\FillPhones");
+#    os.mkdir(temppath + "\FillPhones");
+#file = codecs.open(temppath + '\FillPhones\ADT_conf_raw.html', 'w', encoding = 'utf-8')
+#file.write(page)
+#file.close()
+#with open(temppath + '\FillPhones\ADT_conf_raw.html', 'r', encoding = 'utf-8') as file:
+#    for line in file:
+#        if "Телефоны сотрудников" in line:
+#            start_table = line.find("Телефоны сотрудников")
+#            end_table = line.find("2. Доступ в Интернет/Телефония", start_table - 1)
+#            out = line[start_table:end_table]
+#            with open(temppath + '\FillPhones\ADT_conf_staff_raw.html', 'w', encoding = 'utf-8') as file1:
+#                file1.write(out + "\n")           
+#conf_staff_raw_html = pd.read_html(temppath + '\FillPhones\ADT_conf_staff_raw.html', encoding = 'utf-8')
+#conf_staff_raw_xlsx = conf_staff_raw_html[0].drop(conf_staff_raw_html[0].columns[[0, 4, 6, 7]], axis='columns')
+#conf_staff_raw_xlsx.to_excel(temppath + '\FillPhones\ADT_conf_staff_raw.xlsx', index=False)
+#conf_staff_split_xlsx = conf_staff_raw_xlsx['Ф.И.О.'].str.split(' ',expand=True)
+#conf_staff_split_xlsx.to_html(temppath + '\FillPhones\ADT_conf_staff_split.html', index=False)
+#conf_staff_split_html = pd.read_html(temppath + '\FillPhones\ADT_conf_staff_split.html', encoding = 'utf-8')
+#conf_staff_names = conf_staff_split_html[0].drop(conf_staff_split_html[0].columns[[2]], axis='columns')
+#conf_staff_names.to_excel(temppath + '\FillPhones\ADT_conf_staff_names.xlsx', index=False)
+#conf_staff_info = conf_staff_raw_html[0].drop(conf_staff_raw_html[0].columns[[0, 1, 4, 6, 7]], axis='columns')
+#conf_staff_info.to_excel(temppath + '\FillPhones\ADT_conf_staff_info.xlsx', index=False)
+#conf_staff_login = pd.read_excel(temppath + '\FillPhones\ADT_conf_staff_names.xlsx', encoding = 'utf-8')
+#conf_staff_login['Логин'] = ""
+#conf_staff = conf_staff_login.drop(conf_staff_login.columns[[0, 1]], axis='columns')
+#conf_staff = conf_staff.merge(conf_staff_names, left_index=True, right_index=True)
+#conf_staff = conf_staff.merge(conf_staff_info, left_index=True, right_index=True)
+#conf_staff.to_excel(temppath + '\FillPhones\ADT_conf_staff.xlsx', index=False, header=["Логин", "Фамилия", "Имя", "Внутренний номер", "E-mail", "Моб. номер"])
+
+
+## OTRS parsing and tables preparing
+#browser.get('https://hd.itfb.ru/index.pl?Action=AdminCustomerUser;Nav=adt')
+#s_username = browser.find_element_by_id('User')
+#s_password = browser.find_element_by_id('Password')
+#s_continue = browser.find_element_by_id('LoginButton')
+#s_username.send_keys(str(input('Введите логин: ')))
+#s_password.send_keys(str(getpass('Введите пароль: ')))
+#s_continue.click()
+#company_name = browser.find_element_by_xpath('//*[@id="Search"]')
+#company_search = browser.find_element_by_xpath('//form[contains(@class, "SearchBox")]//button[contains(@title,"Поиск")]')
+#browser.execute_script("arguments[0].click();", company_name)
+#browser.execute_script("arguments[0].value = 'ADT';", company_name);
+#browser.execute_script("arguments[0].click();", company_search)
+#page = browser.page_source
+#otrs_staff_raw_html = pd.read_html(browser.find_element_by_id("CustomerTable").get_attribute('outerHTML'))
+#otrs_staff_raw_xlsx = otrs_staff_raw_html[0].drop(otrs_staff_raw_html[0].columns[[3, 4, 5]], axis='columns')
+#otrs_staff_raw_xlsx.to_excel(temppath + '\FillPhones\ADT_otrs_staff_raw.xlsx', index=False)
+#otrs_staff_split_xlsx = otrs_staff_raw_xlsx['Name - Название'].str.split(' ',expand=True)
+#otrs_staff_split_xlsx.to_html(temppath + '\FillPhones\ADT_otrs_staff_split.html', index=False)
+#otrs_staff_split_html = pd.read_html(temppath + '\FillPhones\ADT_otrs_staff_split.html', encoding = 'utf-8')
+#otrs_staff_names_part1 = otrs_staff_split_html[0].drop(otrs_staff_split_html[0].columns[[0, 2]], axis='columns')
+#otrs_staff_names_part2 = otrs_staff_split_html[0].drop(otrs_staff_split_html[0].columns[[1, 2]], axis='columns')
+#otrs_staff_names_part1.to_excel(temppath + '\FillPhones\ADT_otrs_staff_names_part1.xlsx', index=False)
+#otrs_staff_names_part2.to_excel(temppath + '\FillPhones\ADT_otrs_staff_names_part2.xlsx', index=False)
+#otrs_staff_names = otrs_staff_names_part1.merge(otrs_staff_names_part2, left_index=True, right_index=True)
+#otrs_staff_names.to_excel(temppath + '\FillPhones\ADT_otrs_staff_names.xlsx', header=False, index=False)
+#otrs_staff_login = otrs_staff_raw_html[0].drop(otrs_staff_raw_html[0].columns[[1, 2, 3, 4, 5]], axis='columns')
+#otrs_staff_email = otrs_staff_raw_html[0].drop(otrs_staff_raw_html[0].columns[[0, 1, 3, 4, 5]], axis='columns')
+#otrs_staff = otrs_staff_login.merge(otrs_staff_names, left_index=True, right_index=True)
+#otrs_staff['Внутренний номер'] = ""
+#otrs_staff = otrs_staff.merge(otrs_staff_email, left_index=True, right_index=True)
+#otrs_staff['Моб. номер'] = ""
+#otrs_staff.to_excel(temppath + '\FillPhones\ADT_otrs_staff.xlsx', index=False, header=["Логин", "Фамилия", "Имя", "Внутренний номер", "E-mail", "Моб. номер"])
+#conf_staff = pd.read_excel(temppath + '\FillPhones\ADT_conf_staff.xlsx', encoding = 'utf-8')
+#otrs_staff = pd.read_excel(temppath + '\FillPhones\ADT_otrs_staff.xlsx', encoding = 'utf-8')
+#staff = conf_staff.merge(otrs_staff, on=['Фамилия', 'Имя', 'E-mail'], how = 'outer')
+#staff['Логин_x'] = staff[['Логин_x','Логин_y']].fillna('').sum(axis=1)
+#staff['Внутренний номер_x'] = staff['Внутренний номер_x'].fillna(0).astype(int)
+#staff['Внутренний номер_x'] = staff[['Внутренний номер_x','Внутренний номер_y']].sum(axis=1)
+#staff['Внутренний номер_x'] = staff['Внутренний номер_x'].replace(0, np.nan, regex=True)
+##staff['E-mail_x'] = staff[['E-mail_x','E-mail_y']].fillna('').sum(axis=1)
+#staff['Моб. номер_x'] = staff[['Моб. номер_x','Моб. номер_y']].fillna('').sum(axis=1)
+#staff = staff.drop(staff.columns[[6, 7, 8]], axis='columns')
+#staff = pd.read_excel(temppath + '\FillPhones\ADT_staff.xlsx', encoding = 'utf-8')
+#staff.to_excel(temppath + '\FillPhones\ADT_staff_test.xlsx', index=False, header=["Логин", "Фамилия", "Имя", "Внутренний номер", "E-mail", "Моб. номер"])
+staff = pd.read_excel(temppath + '\FillPhones\ADT_staff_test.xlsx', encoding = 'utf-8')
+print(staff.groupby(['Фамилия', 'Имя'], group_keys=False).agg('sum'))
+staff.to_excel(temppath + '\FillPhones\ADT_staff_test.xlsx', index=False, header=["Логин", "Фамилия", "Имя", "Внутренний номер", "E-mail", "Моб. номер"])
+
+#browser.close()
+#browser.quit()
+
+
+# Deleting temp files
+#os.remove(temppath + '\FillPhones\ADT_conf_raw.html')
+#os.remove(temppath + '\FillPhones\ADT_conf_staff_raw.html')
+#os.remove(temppath + '\FillPhones\ADT_conf_staff_raw.xlsx')
+#os.remove(temppath + '\FillPhones\ADT_conf_staff_split.html')
+#os.remove(temppath + '\FillPhones\ADT_conf_staff_names.xlsx')
+#os.remove(temppath + '\FillPhones\ADT_conf_staff_info.xlsx')
+#os.remove(temppath + '\FillPhones\ADT_otrs_staff_raw.xlsx')
+#os.remove(temppath + '\FillPhones\ADT_otrs_staff_split.html')
+#os.remove(temppath + '\FillPhones\ADT_otrs_staff_names_part1.xlsx')
+#os.remove(temppath + '\FillPhones\ADT_otrs_staff_names_part2.xlsx')
+#os.remove(temppath + '\FillPhones\ADT_otrs_staff_names.xlsx')
